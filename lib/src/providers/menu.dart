a6ceded0902d6ccd1a119f82c11c9a8deb655983
jparada <jparada@edutecno.com>
2021-05-26 08:46:10 -0400

    Future List Diynamic in Widget

--- lib/src/providers/menu.dart
+++ lib/src/providers/menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/services.dart' show rootBundle;
+import 'dart:convert';
 
 class _MenuProvider {
   List<dynamic> menu = [];
@@ -7,10 +8,16 @@ class _MenuProvider {
     leerJson();
   }
 
-  leerJson() {
-    rootBundle.loadString('data/menu_provider.json').then((data) {
-      print(data);
-    });
+  /*Se utilizara Future para  que se construya el widget despues de utilizar el async,
+ utilizas async para esperar a  leer el archivo para cargar y mostrar data 
+  */
+  Future<List<dynamic>> leerJson() async {
+    final respuesta = await rootBundle.loadString('data/menu_provider.json');
+    Map dataMap = json.decode(respuesta);
+    print(dataMap['rutas']);
+    menu = dataMap['rutas'];
+
+    return menu;
   }
 }
 
