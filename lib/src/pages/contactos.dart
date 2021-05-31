3c17ca3c5512b5df76b4a05040270092754606ac
jparada <jparada@edutecno.com>
2021-05-31 15:59:38 -0400

    Form Contact

--- lib/src/pages/contactos.dart
+++ lib/src/pages/contactos.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
 
 //Esta sera la pagina temporal de los menus
-class Contactos extends StatelessWidget {
+class Contactos extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _ContactoState createState() => _ContactoState();
+}
+
+class _ContactoState extends State<Contactos> {
+  String _nombre = '';
+  String _direccion = '';
+  @override
   final estiloText = new TextStyle(fontSize: 20);
   final opciones = ['Contac1', 'Contact2'];
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Contactos'),
+        title: Text('Registrar nuevo vecino'),
         backgroundColor: Colors.orangeAccent,
         actions: <Widget>[
           Container(
@@ -29,21 +35,33 @@ class Contactos extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(
-        child: FadeInImage(
-          image: NetworkImage(
-              'https://scontent-scl2-1.xx.fbcdn.net/v/t31.18172-8/17192646_10212081323814309_3581805654920506777_o.jpg?_nc_cat=103&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=yvfVaC1BEBQAX9WiXLO&_nc_ht=scontent-scl2-1.xx&oh=14db7d27110cf2333d1e46c95f0a9694&oe=60DC08B7'),
-          placeholder: AssetImage('assets/jar-loading.gif'),
-          fadeInDuration: Duration(milliseconds: 200),
-        ),
-      ),
+      body: ListView(
+        padding: EdgeInsets.all(20),
+        children: <Widget>[
+          _nombreVecino(),
+          SizedBox(height: 30.0),
+          Divider(),
+          _direccionVecino(),
+          Divider(),
+          _createVecino(),
+        ],
+        // ),
 
-      //body: ListView(children: _crearOpciones()),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add_location),
-        onPressed: () {
-          Navigator.pop(context);
-        },
+        //  Center(
+        //   child: FadeInImage(
+        //     image: NetworkImage(
+        //         'https://scontent-scl2-1.xx.fbcdn.net/v/t31.18172-8/17192646_10212081323814309_3581805654920506777_o.jpg?_nc_cat=103&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=yvfVaC1BEBQAX9WiXLO&_nc_ht=scontent-scl2-1.xx&oh=14db7d27110cf2333d1e46c95f0a9694&oe=60DC08B7'),
+        //     placeholder: AssetImage('assets/jar-loading.gif'),
+        //     fadeInDuration: Duration(milliseconds: 200),
+        //   ),
+        // ),
+
+        // //body: ListView(children: _crearOpciones()),
+        // floatingActionButton: FloatingActionButton(
+        //   child: Icon(Icons.add_location),
+        //   onPressed: () {
+        //     Navigator.pop(context);
+        //   },
       ),
     );
   }
@@ -62,5 +80,53 @@ class Contactos extends StatelessWidget {
       );
     }).toList();
     return widgets;
+  }
+
+  //Nombre Vecino
+  Widget _nombreVecino() {
+    return TextField(
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(5.0)),
+          counter: Text('Letras ${_nombre.length}'),
+          hintText: 'Nombre vecino',
+          labelText: 'Nombre Vecino',
+          helperText: 'Ingresar nombre completo',
+          suffixIcon: Icon(Icons.accessibility),
+          icon: Icon(Icons.account_circle)),
+      onChanged: (valor) {
+        setState(() {
+          _nombre = valor;
+          print(_nombre);
+        });
+      },
+    );
+  }
+
+  //Apellido Vecino
+  Widget _direccionVecino() {
+    return TextField(
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(5.0)),
+          counter: Text('Letras ${_direccion.length}'),
+          hintText: 'Direccion vecino',
+          labelText: 'Direccion Vecino',
+          helperText: 'Ingresar Direccion completa',
+          suffixIcon: Icon(Icons.home),
+          icon: Icon(Icons.home_filled)),
+      onChanged: (valor) {
+        setState(() {
+          _direccion = valor;
+          print(_direccion);
+        });
+      },
+    );
+  }
+
+  Widget _createVecino() {
+    return ListTile(
+      title: Text('Nombre es: $_nombre'),
+    );
   }
 }
